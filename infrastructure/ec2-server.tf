f6a02aa52e9f59df23f7b5876781a55756bc422b
Nguyen Minh Huan <minhhuan.study@gmail.com>
2025-10-14 16:08:46 +0700

    Update create 4 server for CI/CD

--- infrastructure/ec2-server.tf
+++ infrastructure/ec2-server.tf
@@ -3,18 +3,62 @@ resource "aws_key_pair" "ec2-server" {
   public_key = file("~/.ssh/ec2-server.pub")
 }
 
-resource "aws_instance" "server" {
-  ami                         = "ami-088d74defe9802f14" 
-  instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.ec2-server.key_name
-  vpc_security_group_ids      = [aws_security_group.jenkins-sg.id]
-  associate_public_ip_address = true
+variable "server_definitions" {
+  description = "CI/CD Server"
+  type = map(object({
+    instance_type = string
+    script_file = string
+    tags = map(string)
+  }))
 
-  user_data = file("ec2-install/jenkins.sh")
+  default = {
+    "jenkins" = {
+      instance_type = "t3.small"
+      script_file   = "ec2-install/jenkins.sh"
+      tags = {
+        Name = "Jenkins"
+      }
+    }
 
-  tags = {
-    Name = "Jenkins"
+    "sonar-qube" = {
+      instance_type = "t3.medium"
+      script_file   = "ec2-install/sonar-qube.sh"
+      tags = {
+        Name = "SonarQube"
+      }
+    }
+
+    "nexus" = {
+      instance_type = "t3.medium"
+      script_file   = "ec2-install/nexus.sh"
+      tags = {
+        Name = "Nexus"
+      }
+    }
+
+    grafana = {
+      instance_type = "t3.small"
+      script_file   = "ec2-install/grafana.sh"
+      tags = {
+        Name = "Grafana"
+      }
+    }
   }
+  
+}
+
+resource "aws_instance" "server" {
+  ami                         = "ami-088d74defe9802f14"  
+  for_each                    = var.server_definitions
+
+  instance_type               = each.value.instance_type
+  key_name                    = aws_key_pair.ec2-server.key_name
+  vpc_security_group_ids      = [aws_security_group.ec2-server.id]
+  subnet_id                   = aws.subnet.public.id
+
+  associate_public_ip_address = true
+  user_data                   = file(each.value.script_file)
+  tags                        = merge(each.value.tags, { "Project" = "ECS-Deployer" })
 }
 
 
